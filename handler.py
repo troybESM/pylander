2ae66b5800bb489ad3ee4bf045ba56d6b5fad224
troybESM <troybrewer1167@gmail.com>
2020-12-13 14:29:11 -0500

    Added disney and marvel lists

--- handler.py
+++ handler.py
@@ -22,6 +22,10 @@ def get_random_word(channel):
         wordList = open('adult.list').read().split()
     elif channel.startswith('starwars'):
         wordList = open('starwars.list').read().split()
+    elif channel.startswith('disney'):
+        wordList = open('disney.list').read().split()
+    elif channel.startswith('marvel'):
+        wordList = open('marvel.list').read().split()
     else:        
         wordList = open('words.list').read().split()    
     random_word = random.choice(wordList)
